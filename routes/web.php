41b21d27163a9ad0096b8b98b2f1d3940d8f251f
MehdiDgrs <Mehdigital@outlook.com>
2023-10-22 13:55:45 +0200

    created feedbacks table and Feedback model

--- routes/web.php
+++ routes/web.php
@@ -45,5 +45,9 @@ Route::resource('/reviews', ReviewController::class)
     ->only(['index', 'store'])
     ->middleware(['auth', 'verified']);
 
-
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 require __DIR__.'/auth.php';
